2221fb112ca1c8825bc7b7685eed19372e79d4c2
minsung37 <tampang99@naver.com>
2022-05-21 11:11:59 +0900

    5/21

--- Ch9/20-1.py
+++ Ch9/20-1.py
@@ -1,17 +1,18 @@
 # https://leetcode.com/problems/valid-parentheses/
-# 48ms 13.9MB
+# 32ms 13.9MB
 class Solution:
     @staticmethod
     def isValid(s: str) -> bool:
         parenthesis = [[")", "("], ["}", "{"], ["]", "["]]
         s, stack = list(s), []
-        stack.append(s.pop())
         while s:
+            # s에서 하나씩 빼서 스택에 넣기
             stack.append(s.pop())
-            print(stack[-2:])
+            # 스택 뒤 2개가 괄호열이면 2개를 스택에서 팝한다.
             if stack[-2:] in parenthesis:
                 stack.pop()
                 stack.pop()
+        # 전부 pop된경우에는 stack의 길이가 0이다
         if len(stack) == 0:
             return True
         return False
