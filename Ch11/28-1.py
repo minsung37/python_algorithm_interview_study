5b94a46ce3083a9e8203e8c62b680380086f8f1a
minsung37 <tampang99@naver.com>
2022-05-31 02:12:08 +0900

    5/31

--- Ch11/28-1.py
+++ Ch11/28-1.py
@@ -17,27 +17,29 @@ class MyHashMap:
 
     def put(self, key: int, value: int) -> None:
         index = key % self.size
-        # 인덱스에 노드가 없다면 삽입 후 종료
+        # 인덱스 빈 경우
         if self.table[index].value is None:
             self.table[index] = ListNode(key, value)
             return
-        # 인덱스에 노드가 존재하는 경우 연결 리스트 처리
+        # 연결 리스트 만들기
         p = self.table[index]
         while p:
+            # 이미 존재하는키 업데이트
             if p.key == key:
                 p.value = value
                 return
+            # 다음이 널값이면삽입
             if p.next is None:
                 break
             p = p.next
         p.next = ListNode(key, value)
 
-    # 조회
     def get(self, key: int) -> int:
         index = key % self.size
+        # 노드 없을때
         if self.table[index].value is None:
             return -1
-        # 노드가 존재할 때 일치하는 키 탐색
+        # 키 일치할때 값 출력
         p = self.table[index]
         while p:
             if p.key == key:
@@ -45,7 +47,6 @@ class MyHashMap:
             p = p.next
         return -1
 
-    # 삭제
     def remove(self, key: int) -> None:
         index = key % self.size
         if self.table[index].value is None:
@@ -53,7 +54,10 @@ class MyHashMap:
         # 인덱스의 첫 번째 노드일 때 삭제 처리
         p = self.table[index]
         if p.key == key:
-            self.table[index] = ListNode() if p.next is None else p.next
+            if p.next is None:
+                self.table[index] = ListNode()
+            else:
+                self.table[index] = p.next
             return
         # 연결 리스트 노드 삭제
         prev = p
