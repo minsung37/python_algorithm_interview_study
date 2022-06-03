799a55d4bdfaee12e81bf0e7f0c5f13235629b08
minsung37 <tampang99@naver.com>
2022-06-04 03:32:32 +0900

    6/4

--- Ch12/32-1.py
+++ Ch12/32-1.py
@@ -8,21 +8,17 @@ class Solution:
     def numIslands(grid: list[list[str]]) -> int:
         dx, dy, count = [1, -1, 0, 0], [0, 0, 1, -1], 0
 
-    # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
         def bfs(x, y):
             queue = deque()
             queue.append((x, y))
             grid[x][y] = "0"
-            # 큐가 빌때까진 반복
             while queue:
                 x, y = queue.popleft()
-                # 4방향 탐색
                 for i in range(4):
                     nx = x + dx[i]
                     ny = y + dy[i]
                     if nx < 0 or ny < 0 or nx >= len(grid) or ny >= len(grid[0]):
                         continue
-                    # 방문한경우
                     if grid[nx][ny] == "1":
                         grid[nx][ny] = "0"
                         queue.append((nx, ny))
