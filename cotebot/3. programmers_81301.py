f263a2e4f79640c1b3f37c60e66d24251c9ad673
minsung37 <tampang99@naver.com>
2022-06-11 22:17:49 +0900

    6/11

---
+++ cotebot/3. programmers_81301.py
@@ -0,0 +1,26 @@
+# https://programmers.co.kr/learn/courses/30/lessons/81301
+
+
+
+
+
+
+def solution(s):
+    number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    result, stack = [], ""
+    for i in s:
+        try:
+            i = int(i)
+            if len(stack) != 0:
+                result.append(str(number.index(stack)))
+                stack = ""
+            result.append(str(i))
+        except:
+            stack = stack + i
+            if stack in number:
+                result.append(str(number.index(stack)))
+                stack = ""
+    if stack:
+        result.append(str(number.index(stack)))
+    s = "".join(result)
+    return int(s)
---
+++ programmers/43105.py
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43105
+# 정수삼각형
+# def solution(triangle):
+#     answer = 0
+#     for i in range(len(triangle) - 1):
+#         triangle[i + 1] =
+#     return answer
